af6c9682d3c032ee0db3653095c70d88024673c4
Jackson Volante <jvolante@anduril.com>
2026-05-01 09:58:05 -0400

    tweaks

--- bash_env.sh
+++ bash_env.sh
@@ -1,4 +1,5 @@
 alias oc='opencode'
 alias ocm='opencode --model $OPENCODE_MEDIUM_MODEL'
+alias ocs='opencode --model $OPENCODE_SMALL_MODEL'
 alias occ='opencode --agent chat --model $OPENCODE_MEDIUM_MODEL'
 export OPENCODE_EXPERIMENTAL_BASH_DEFAULT_TIMEOUT_MS=$((60 * 60 * 1000))
